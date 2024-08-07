28fbaec68194a843505ea4946e6b5e2286fcb3cd
chaiyanan-s <chyn.seekhachon@gmail.com>
2024-08-07 09:27:59 +0700

    fix

--- lab5-angular-app/src/app/components/ngfor/ngfor.component.ts
+++ lab5-angular-app/src/app/components/ngfor/ngfor.component.ts
@@ -8,7 +8,8 @@ import { Component } from '@angular/core';
 export class NgforComponent {
 
   employee: any
-    = [{ "id": "1", "employee_name": "WdqBvFe", "employee_salary": "797", "employee_age": "36" },
+    = [
+    { "id": "1", "employee_name": "WdqBvFe", "employee_salary": "797", "employee_age": "36" },
     { "id": "1925", "employee_name": "Menaka6", "employee_salary": "24501", "employee_age": "24501" },
     { "id": "1969", "employee_name": "2381", "employee_salary": "123", "employee_age": "23" },
     { "id": "1970", "employee_name": "6132", "employee_salary": "123", "employee_age": "23" },
@@ -21,5 +22,6 @@ export class NgforComponent {
     { "id": "1997", "employee_name": "test-654", "employee_salary": "123", "employee_age": "23" },
     { "id": "1999", "employee_name": "test-127", "employee_salary": "123", "employee_age": "23" },
     { "id": "2001", "employee_name": "test-301", "employee_salary": "123", "employee_age": "23" },
-    { "id": "2003", "employee_name": "1769", "employee_salary": "123", "employee_age": "23" }]
+    { "id": "2003", "employee_name": "1769", "employee_salary": "123", "employee_age": "23" }
+  ]
 }
